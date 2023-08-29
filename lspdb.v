d718c33325c2dc5200dacb5f7222243ed4cfb166
gitlab.com/pepa65 <pepa65@passchier.net>
2023-08-29 12:51:41 +0700

    Full error checking

--- lspdb.v
+++ lspdb.v
@@ -5,10 +5,18 @@
 import os
 
 fn getquad(db []u8, addr u32) u32 {
+	if addr+4 > db.len {
+		eprintln('Error: PureDB database is corrupted')
+		exit (3)
+	}
 	return u32(db[addr])<<24 + u32(db[addr+1])<<16 + u32(db[addr+2])<<8 + db[addr+3]
 }
 
 fn out(db []u8, addr u32, len u32) {
+	if addr+len > db.len {
+		eprintln('Error: PureDB database is corrupted')
+		exit (4)
+	}
 	for i in addr..addr+len {
 		print(db[i].ascii_str())
 	}
@@ -16,17 +24,18 @@ fn out(db []u8, addr u32, len u32) {
 
 fn main(){
 	if os.args.len != 2 {
-		eprintln('Error: a PureDB .pdb file needed as an argument to print out')
-		exit(1)
+		eprintln('lspdb - Print all "key: value" pairs in a given PureDB .pdb file')
+		eprintln('Usage: '+os.args[0]+' <PureDB.pdb>')
+		exit(0)
 	}
 	file := os.args[1]
 	db := os.read_bytes(file) or {
 		eprintln('Error: file "${file}" not found')
-		exit(2)
+		exit(1)
 	}
 	if db[0] != `P` && db[1] != `D` && db[2] != `B` && db[3] != `2` {
 		eprintln('Error: file magic wrong for a PureDB file')
-		exit(3)
+		exit(2)
 	}
 	mut addr := getquad(db, 1028)
 	mut len := u32(0)
